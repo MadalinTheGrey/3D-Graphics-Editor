6b8252f5b159e5afe53e895bd62aed382fd09637
MadalinTheGrey <150152239+MadalinTheGrey@users.noreply.github.com>
2023-12-01 20:45:14 +0200

    Update Initialization.h

--- Initialization.h
+++ Initialization.h
@@ -10,6 +10,6 @@ void InitializeMenuPanels()
 	setfillstyle(SOLID_FILL, COLOR(35, 35, 35));
 	bar(0, 0, getmaxx(), 30);
 	setfillstyle(SOLID_FILL, COLOR(20, 20, 20));
-	bar(1080, 30, getmaxx(), getmaxy());
+	bar(getmaxx() - 200, 30, getmaxx(), getmaxy());
 	bar(0, 100, 40, getmaxy() - 100);
 }
