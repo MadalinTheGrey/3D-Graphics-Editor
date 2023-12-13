bd6843505e9ed89913eb571c7646d17d09b0c343
MadalinTheGrey <150152239+MadalinTheGrey@users.noreply.github.com>
2023-12-13 17:10:57 +0200

    Rotirea si zoom - tastatura
    Trebuie conectate cu interfata

--- main.cxx
+++ main.cxx
@@ -3,38 +3,56 @@
 #include "graphics.h"
 #include "Initialization.h"
 #include "Corp.h"
+#include "GlobalVariables.h"
 
 using namespace std;
 
 int main()
 {
     InitializeWindow();
-    /*Punct3D pcte[] = {
+    Punct3D pcte[] = {
+        Punct3D(100, 100, 100),
+        Punct3D(100, 200, 100),
+        Punct3D(200, 200, 100),
+        Punct3D(200, 100, 100),
+        Punct3D(100, 100, 200),
+        Punct3D(100, 200, 200),
         Punct3D(200, 200, 200),
-        Punct3D(200, 400, 200),
-        Punct3D(400, 400, 200),
-        Punct3D(400, 200, 200),
-        Punct3D(200, 200, 400),
-        Punct3D(200, 400, 400),
-        Punct3D(400, 400, 400),
-        Punct3D(400, 200, 400)
+        Punct3D(200, 100, 200)
     };
     Linie lnii[] = {
-        Linie(pcte[0], pcte[1]),
-        Linie(pcte[1], pcte[2]),
-        Linie(pcte[2], pcte[3]),
-        Linie(pcte[3], pcte[0]),
-        Linie(pcte[4], pcte[5]),
-        Linie(pcte[5], pcte[6]),
-        Linie(pcte[6], pcte[7]),
-        Linie(pcte[7], pcte[4]),
-        Linie(pcte[4], pcte[0]),
-        Linie(pcte[5], pcte[1]),
-        Linie(pcte[6], pcte[2]),
-        Linie(pcte[7], pcte[3])
+        Linie(0, 1),
+        Linie(1, 2),
+        Linie(2, 3),
+        Linie(3, 0),
+        Linie(4, 5),
+        Linie(5, 6),
+        Linie(6, 7),
+        Linie(7, 4),
+        Linie(4, 0),
+        Linie(5, 1),
+        Linie(6, 2),
+        Linie(7, 3)
     };
     Corp C(8, 12, 2, pcte, lnii);
-    C.AfisareCorp();*/
-    getch();
+    setviewport(GB.vp_tl_x, GB.vp_tl_y, GB.vp_dr_x, GB.vp_dr_y, 0);
+    C.AfisareCorp();
+
+    //Rotire
+    char tasta;
+    do
+    {
+        tasta = getch();
+        if (tasta == 'e') C.RotesteXPoz();
+        else if (tasta == 'w') C.RotesteXNeg();
+        else if (tasta == 'd') C.RotesteYPoz();
+        else if (tasta == 's') C.RotesteYNeg();
+        else if (tasta == 'x') C.RotesteZNeg();
+        else if (tasta == 'c') C.RotesteZPoz();
+        else if (tasta == 'p') GB.zoom += 0.2;
+        else if (tasta == 'm') GB.zoom -= 0.2;
+        clearviewport();
+        C.AfisareCorp();
+    } while (tasta != 'q');
     closegraph();
 }
