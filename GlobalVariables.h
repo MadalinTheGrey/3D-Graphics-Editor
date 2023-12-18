d6e8fffd5c3ced34e398989097a6ba4a83eb4d38
MadalinTheGrey <150152239+MadalinTheGrey@users.noreply.github.com>
2023-12-18 23:16:50 +0200

    Update GlobalVariables.h

--- GlobalVariables.h
+++ GlobalVariables.h
@@ -1,20 +1,19 @@
-#include "graphics.h"
-#pragma once
-using namespace std;
+#ifndef INC_3D_EDITOR_V2_GLOBALVARIABLES_H
+#define INC_3D_EDITOR_V2_GLOBALVARIABLES_H
 
-static class GlobalVariables{
-public:
-    unsigned int window_width = 1280;
-    unsigned int window_height = 720;
-    char window_name[10] = "Editor 3D";
-    int window_bg_color = BLACK;
-    int window_startPosX = 100;
-    int window_startPosY = 50;
-    int MAPPED_COLORS[1280][720] = {0};
-    int MAPPED_BUTTONS[1280][720] =  {0};
-    int dist_obs = 600; // distanta de la observator la planul ecranului
-    double default_angle = 0.1; // in radiani
-    int vp_tl_x = 40, vp_tl_y = 30; // coordonate implicite stanga sus viewport
-    int vp_dr_x = 1080, vp_dr_y = 720; // coordonate implicite dreapta jos viewport
-    double zoom = 1;
-}GB;
+#define BLACK 0x000000
+
+static int windowWidth = 1280;
+static int windowHeight = 720;
+static char windowName[100] = "3D Editor";
+static int windowBgColor = BLACK;
+static int windowPosX = 100;
+static int windowPosY = 50;
+static int dist_obs = 600;
+static double zoom = 1;
+static double default_angle = 0.01;
+static double offsetX = 0, offsetY = 0; //modifica pozitia corpurilor in scena
+int vp_tl_x = 40, vp_tl_y = 30; // coordonate implicite stanga sus viewport
+int vp_dr_x = 1080, vp_dr_y = 720; // coordonate implicite dreapta jos viewport
+
+#endif //INC_3D_EDITOR_V2_GLOBALVARIABLES_H
