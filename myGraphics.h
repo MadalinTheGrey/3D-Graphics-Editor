391ab9e22b71013a7c534605f11fbbc910a06cab
MadalinTheGrey <150152239+MadalinTheGrey@users.noreply.github.com>
2023-12-18 23:18:44 +0200

    Update myGraphics.h

--- myGraphics.h
+++ myGraphics.h
@@ -1,40 +1,52 @@
-#include <graphics.h>
-#include <winbgim.h>
+#ifndef INC_3D_EDITOR_V2_MYGRAPHICS_H
+#define INC_3D_EDITOR_V2_MYGRAPHICS_H
+#include "graphics.h"
 #include "GlobalVariables.h"
-
-static void CreateStartWindow(int width, int height, char name[] = "Window", int bg_color = BLACK, int startPosX = 0, int startPozY = 0) {
-    int gdriver=0, gmode, errorcode;
+static void InitializeWindow(int width = windowWidth, int height = windowHeight, char name[] = windowName, int bgColor = windowBgColor, int posX = windowPosX, int posY = windowPosY) {
+    int gdriver = 0, gmode = 0, errorCode;
     detectgraph(&gdriver, &gmode);
     initgraph(&gdriver, &gmode, "");
-    errorcode = graphresult();
+    errorCode = graphresult();
     //error
-     if (errorcode != grOk) {
-        printf("Graphics error: %s\n", grapherrormsg(errorcode));
-        printf("Press any key to halt:");
-        getch();
-        exit(1);
-   }
-   closegraph();
-   initwindow(width, height, name, startPosX, startPozY);
-   setbkcolor(bg_color);
-   cleardevice();
-   for(int i=0; i<=height; ++i) {
-      for(int j=0; j<=width; ++j) {
-         GB.MAPPED_COLORS[i][j] = bg_color;
-      }
-   }
+    if (errorCode != grOk) {
+        printf("Graphics error: %s\n", grapherrormsg(errorCode));
+    }
+    closegraph();
+    initwindow(width, height, name, posX, posY);
+    setbkcolor(bgColor);
+    cleardevice();
 }
-void DrawStaticRectangle(int x1, int y1, int x2, int y2, int color = WHITE) {
-   setfillstyle(SOLID_FILL, color);
-   bar(x1, y1, x2, y2);
-   for(int i=x1; i<=x2; ++i) {
-      for(int j=y1; j<=y2; ++j) {
-            GB.MAPPED_COLORS[i][j] = color;
-      }
-   }
+static void drawFilledRectangle(int x1, int y1, int x2, int y2, int color = BLACK, int style = SOLID_FILL) {
+    if (x1 < 0)
+        x1 = 0;
+    if (x2 > getmaxx())
+        x2 = getmaxx();
+    if (y1 < 0)
+        y1 = 0;
+    if (y2 > getmaxy())
+        y2 = getmaxy();
+    setfillstyle(SOLID_FILL, color);
+    bar(x1, y1, x2, y2);
 }
-void WriteText(int x, int y, char *text, int size = 1) {
-    //TODO new implementation
-   settextstyle(8,HORIZ_DIR, size);
-   outtextxy(x, y, text);
+static void drawLine(int x1, int y1, int x2, int y2, int color = WHITE, int thickness = 1, int style = SOLID_LINE) {
+    setcolor(color);
+    setlinestyle(style, 0, thickness);
+    line(x1, y1, x2, y2);
 }
+static void writeText(int x, int y, char text[], int color = BLACK, int size = 1, int vi = 0) {
+    //TODO for string too
+    setcolor(color);
+    settextstyle(1, 0, 0);
+    setusercharsize(1, 2, 1, 2);
+    int x1 = x;
+    int y1 = y - textheight(text);
+    int x2 = x + textwidth(text);
+    int y2 = y + textheight(text);
+    if (vi == 0) { //Daca trebuie sa modificam culoarea bg astfel incat sa nu apara chenar negru
+        setviewport(x1, y1, x2, y2, 1);
+        setbkcolor(COLOR(35, 35, 35));
+    }
+    outtextxy(0, 0, text);
+    setviewport(0, 0, getmaxx(), getmaxy(), 1);
+}
+#endif //INC_3D_EDITOR_V2_MYGRAPHICS_H
