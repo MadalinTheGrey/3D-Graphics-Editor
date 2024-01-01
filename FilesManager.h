d3c54a1253a2a9bc98d218bdc84979ef33d6e404
MadalinTheGrey <150152239+MadalinTheGrey@users.noreply.github.com>
2024-01-01 15:58:27 +0200

    Combinat cu patch Andrei

--- FilesManager.h
+++ FilesManager.h
@@ -4,57 +4,43 @@
 #include "graphics.h"
 #include <fstream>
 #include "Corp.h"
+#include "GlobalVariables.h"
 using namespace std;
-Corp readFromFile(char path[]) {
-    Punct3D pcte[] = {
-            Punct3D(100, 100, 100),
-            Punct3D(100, 200, 100),
-            Punct3D(200, 200, 100),
-            Punct3D(200, 100, 100),
-            Punct3D(100, 100, 200),
-            Punct3D(100, 200, 200),
-            Punct3D(200, 200, 200),
-            Punct3D(200, 100, 200)
-    };
-    Linie lnii[] = {
-            Linie(0, 1),
-            Linie(1, 2),
-            Linie(2, 3),
-            Linie(3, 0),
-            Linie(4, 5),
-            Linie(5, 6),
-            Linie(6, 7),
-            Linie(7, 4),
-            Linie(4, 0),
-            Linie(5, 1),
-            Linie(6, 2),
-            Linie(7, 3)
-    };
-    Corp C(8, 12, 2, pcte, lnii, "georgel");
-    return C;
-    /**ifstream fin(path);
-    string read, r1, r2, r3;
-    Corp corp = Corp();
-    Linie linii[100]; int indexLinii = 0;
-    Punct3D puncte[100]; int indexPuncte = 0;
-    if(!fin.is_open()) {
+void readFromFile(string path) {
+    ifstream fin(path.c_str());
+    Scena scena;
+    if (!fin.is_open()) {
         printf("Error opening file!\n");
-        return corp;
+        return;
     }
-    while(fin >> read) {
-        if(read == "Punct:") {
-            fin >> r1 >> r2 >> r3;
-            puncte[indexPuncte++] = {stoi(r1), stoi(r2), stoi(r3)};
-        } else if(read == "Linie:") {
-            fin >> r1 >> r2;
-            linii[indexLinii++] = {stoi(r1), stoi(r2)};
+    else {
+        vector<Corp> corpuri;
+        string read, r1, r2, r3;
+        while (fin >> read) {
+            if (read == "Corp:") {
+                Corp corp;
+                fin >> read;
+                corp.name = read;
+                vector<Punct3D> puncte;
+                vector<Linie> linii;
+                while (read != "EndCorp") {
+                    fin >> read;
+                    if (read == "Punct:") {
+                        fin >> r1 >> r2 >> r3;
+                        puncte.push_back(*new Punct3D(stoi(r1), stoi(r2), stoi(r3)));
+                    }
+                    else if (read == "Linie:") {
+                        fin >> r1 >> r2;
+                        linii.push_back(*new Linie(stoi(r1), stoi(r2)));
+                    }
+                }
+                corp.puncte = puncte;
+                corp.linii = linii;
+                scena.corpuri.push_back(corp);
+            }
         }
+        S = Scena(scena);
     }
-    corp.puncte = puncte;
-    corp.linii = linii;
-    corp.nr_puncte = indexPuncte;
-    corp.nr_linii = indexLinii;
-    return corp;**/
 }
 
 #endif //INC_3D_EDITOR_V2_FILESMANAGER_H
