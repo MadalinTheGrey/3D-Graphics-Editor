af151476ef8a2eca62e6d5d8871bcad39095ee71
MadalinTheGrey <150152239+MadalinTheGrey@users.noreply.github.com>
2024-01-01 15:54:23 +0200

    Combinat cu patch Andrei

--- Render3D.h
+++ Render3D.h
@@ -6,6 +6,9 @@
 #include "Corp.h"
 #include "FilesManager.h"
 
+int renderAgain = 0;
+int currentWindow = 1;
+
 void render3D() {
     setviewport(vp_tl_x, vp_tl_y, vp_dr_x, vp_dr_y, 1);
     setbkcolor(BLACK);
@@ -15,11 +18,11 @@ void render3D() {
     while(true) {
         if(GetAsyncKeyState(VK_ESCAPE) != 0)
             break;
-        else if(GetAsyncKeyState(VK_LEFT) || GetAsyncKeyState(VK_RIGHT) || GetAsyncKeyState(VK_UP) || GetAsyncKeyState(VK_DOWN)
-                || GetAsyncKeyState(0x57) || GetAsyncKeyState(0x53) || GetAsyncKeyState(	0x41) || GetAsyncKeyState(0x44) ||
-                GetAsyncKeyState(0x5A) || GetAsyncKeyState(0x58) || GetAsyncKeyState(0x46) || GetAsyncKeyState(0x47)
-                || GetAsyncKeyState(0x31) || GetAsyncKeyState(0x32) || GetAsyncKeyState(0x33) || GetAsyncKeyState(0x34)
-                || GetAsyncKeyState(0x35) || GetAsyncKeyState(0x36)) {
+        else if ((GetAsyncKeyState(VK_LEFT) || GetAsyncKeyState(VK_RIGHT) || GetAsyncKeyState(VK_UP) || GetAsyncKeyState(VK_DOWN)
+            || GetAsyncKeyState(0x57) || GetAsyncKeyState(0x53) || GetAsyncKeyState(0x41) || GetAsyncKeyState(0x44) ||
+            GetAsyncKeyState(0x5A) || GetAsyncKeyState(0x58) || GetAsyncKeyState(0x46) || GetAsyncKeyState(0x47)
+            || GetAsyncKeyState(0x31) || GetAsyncKeyState(0x32) || GetAsyncKeyState(0x33) || GetAsyncKeyState(0x34)
+            || GetAsyncKeyState(0x35) || GetAsyncKeyState(0x36) || renderAgain) && currentWindow) {
             if(GetAsyncKeyState(VK_LEFT)) {
                 for(auto& C : S.corpuri)
                     for(auto &P : C.puncte)
@@ -57,41 +60,32 @@ void render3D() {
             } if(GetAsyncKeyState(0x47)) { //G
                 zoom-=0.01;
             } if(GetAsyncKeyState(0x31)) { //1
-                for (auto& C : S.corpuri)
-                    if(C.selected) C.RotesteXPoz();
+                for(auto &i : S.corpuri_selectate)
+                    S.corpuri[i].RotesteXPoz();
             } if(GetAsyncKeyState(0x32)) { //2
-                for (auto& C : S.corpuri)
-                    if(C.selected) C.RotesteXNeg();
+                for(auto &i : S.corpuri_selectate)
+                    S.corpuri[i].RotesteXNeg();
             } if(GetAsyncKeyState(0x33)) { //3
-                for (auto& C : S.corpuri)
-                    if(C.selected) C.RotesteYPoz();
+                for (auto& i : S.corpuri_selectate)
+                    S.corpuri[i].RotesteYPoz();
             } if(GetAsyncKeyState(0x34)) { //4
-                for (auto& C : S.corpuri)
-                    if(C.selected) C.RotesteYNeg();
+                for (auto& i : S.corpuri_selectate)
+                    S.corpuri[i].RotesteYNeg();
             } if(GetAsyncKeyState(0x35)) { //5
-                for (auto& C : S.corpuri)
-                    if(C.selected) C.RotesteZPoz();
+                for (auto& i : S.corpuri_selectate)
+                    S.corpuri[i].RotesteZPoz();
             } if(GetAsyncKeyState(0x36)) { //6
-                for (auto& C : S.corpuri)
-                    if(C.selected) C.RotesteZNeg();
+                for (auto& i : S.corpuri_selectate)
+                    S.corpuri[i].RotesteZNeg();
             }
             clearviewport();
             //renderAdditionallyLines(0, 0, getmaxx(), getmaxy(), offsetX, offsetY, zoom);
             S.IncarcaScena();
+            if (renderAgain)
+                renderAgain = 0;
         }
         delay(1);
     }
-}
-
-void LoadFromFile()
-{
-    std::vector<Corp> corpuri;
-    Corp corp;
-    corp = readFromFile("D:\\Andrei\\Desktop\\My Projects\\3D Editor V2\\SavedObjects\\Corp1.txt");
-    corpuri.push_back(corp);
-    Scena Scene(corpuri);
-    S = Scene;
-    render3D();
 }
 
 #endif //INC_3D_EDITOR_V2_RENDER3D_H
