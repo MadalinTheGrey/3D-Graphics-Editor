f062dfac2901c142324658702eb65be653f44087
MadalinTheGrey <150152239+MadalinTheGrey@users.noreply.github.com>
2024-01-08 14:14:27 +0200

    Final patch

--- MouseController.h
+++ MouseController.h
@@ -4,7 +4,6 @@
 #include "EditObject.h"
 using namespace std;
 void checkMouseClick(int x, int y) {
-    cout << x << " " << y << "\n";
     bool is_button = false;
     //pasul la care este user-ul in desenarea unei forme simple (0 = neapelat/terminat, 1 = pozitionat primul punct)
     static int form_draw_step = 0;
@@ -43,29 +42,25 @@ void checkMouseClick(int x, int y) {
             if (buttons[last_clicked_button].name == "Draw Line" && S.corpuri_selectate.size() == 1)
             {
                 int i = S.corpuri_selectate[0];
-                int z_pos = 100; //pozitia pe z pe care o are sectiunea pe care desenam (ar trebui preluata de undeva)
-                S.UserDrawLine(x, y, form_draw_step, z_pos, i);
+                S.UserDrawLine(x, y, form_draw_step, selectedStrat, i);
                 form_draw_step = 1 - form_draw_step;
             }
             else if (buttons[last_clicked_button].name == "Draw rectangle" && S.corpuri_selectate.size() == 1)
             {
                 int i = S.corpuri_selectate[0];
-                int z_pos = 100; //pozitia pe z pe care o are sectiunea pe care desenam (ar trebui preluata de undeva)
-                S.UserDrawRectangle(x, y, form_draw_step, z_pos, i);
+                S.UserDrawRectangle(x, y, form_draw_step, selectedStrat, i);
                 form_draw_step = 1 - form_draw_step;
             }
             else if (buttons[last_clicked_button].name == "Draw circle" && S.corpuri_selectate.size() == 1)
             {
                 int i = S.corpuri_selectate[0];
-                int z_pos = 100; //pozitia pe z pe care o are sectiunea pe care desenam (ar trebui preluata de undeva)
-                S.UserDrawCircle(x, y, form_draw_step, z_pos, i);
+                S.UserDrawCircle(x, y, form_draw_step, selectedStrat, i);
                 form_draw_step = 1 - form_draw_step;
             }
             else if (buttons[last_clicked_button].name == "Add point" && S.corpuri_selectate.size() == 1)
             {
                 int i = S.corpuri_selectate[0];
-                int z_pos = 100; //pozitia pe z pe care o are sectiunea pe care desenam (ar trebui preluata de undeva)
-                S.UserAddPoint(x, y, z_pos, i);
+                S.UserAddPoint(x, y, selectedStrat, i);
             }
             else if (buttons[last_clicked_button].name == "Link points" && S.corpuri_selectate.size() == 1)
             {
@@ -78,7 +73,14 @@ void checkMouseClick(int x, int y) {
         {
             S.ChangeSelected(x, y);
         }
-        if (form_draw_step == 0) last_clicked_button = 0;
+        if (form_draw_step == 0 && last_clicked_button != 0)
+        {
+            setviewport(0, 0, getmaxx(), getmaxy(), 0);
+            drawLine(buttons[last_clicked_button].x1, buttons[last_clicked_button].y1, buttons[last_clicked_button].x2, buttons[last_clicked_button].y1, COLOR(118, 118, 118), 1);
+            drawLine(buttons[last_clicked_button].x1, buttons[last_clicked_button].y2, buttons[last_clicked_button].x2, buttons[last_clicked_button].y2, COLOR(118, 118, 118), 1);
+            setviewport(vp_tl_x, vp_tl_y, vp_dr_x, vp_dr_y, 1);
+            last_clicked_button = 0;
+        }
     }
     if (S.corpuri_selectate.size() == 1 && !editButtonSelectable && !editMode) {
         setviewport(0, 0, 1280, 720, 1);
@@ -93,12 +95,12 @@ void checkMouseClick(int x, int y) {
         setbkcolor(BLACK);
     }
     if (editMode && S.corpuri_selectate.size() == 1) {
-        for (auto btn : strats) {
+        for (auto& btn : strats) {
             if (x > btn.x1 && x < btn.x2 && y > btn.y1 && y < btn.y2) {
                 btn.onClick();
             }
         }
-        for (auto btn : menuStratsButtons) {
+        for (auto& btn : menuStratsButtons) {
             if (x > btn.x1 && x < btn.x2 && y > btn.y1 && y < btn.y2) {
                 btn.onClick();
             }
